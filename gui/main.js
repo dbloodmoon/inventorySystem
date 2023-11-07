86fa82d2ae306709427a20fdb2551f54fabb3217
Daniel Molina <70828221+dbloodmoon@users.noreply.github.com>
2023-11-07 15:38:01 -0400

    Add files via upload

--- gui/main.js
+++ gui/main.js
@@ -1,13 +1,23 @@
 const apiURL = "http://localhost:8000/api/v1/";
 const navItems = document.querySelectorAll(".nav-item");
 const render = document.querySelector(".main-container");
+const prevButton = document.querySelector("#prev-button");
+const nextButton = document.querySelector("#next-button");
+const navPagination = document.querySelector(".nav-pagination");
 const paginationButton = document.querySelectorAll(".page-item");
+const userForm = document.querySelector("#usuarioForm");
+const passwordForm = document.querySelector("#passwordForm");
+const mainSection = document.querySelector("#main-section");
+const loginSection = document.querySelector("#login-section");
+const btnAdmin = document.querySelector("#btnAdmin");
+const btnLogin = document.querySelector("#btnLogin");
 const tableHead = document.querySelector("#tableHead");
 const tableBody = document.querySelector("#tableBody");
-const elementsPerPage = 1;
+const elementsPerPage = 10;
 let actualPage = null;
+let totalPages = 0;
+let page = 1;
 let item = [];
-const totalPages = Math.ceil(item.length / elementsPerPage);
 
 const getItemFromAPI = (URL) => {
   if (item.length > 0) {
@@ -18,7 +28,7 @@ const getItemFromAPI = (URL) => {
     .then((res) => res.json())
     .then((data) => {
       item.push(data);
-      console.log(item);
+      //  console.log(item);
     });
 };
 
@@ -40,8 +50,6 @@ const insertTableContent = () => {
     const formatArray = (textArray = textContent.split(","));
 
     formatArray.forEach((text) => {
-      console.log(Object.keys(item[0][key]).length);
-
       if (
         cont === Object.keys(item[0][key]).length &&
         actualPage !== "departamento"
@@ -57,25 +65,45 @@ const insertTableContent = () => {
     });
 
     tableBody.appendChild(elementTr);
+    totalPages = Math.ceil(item[0].length / elementsPerPage);
   }
 };
 
-const showPages = (page = 1) => {
+const setCurrentPage = (pageNum) => {
+  page = pageNum;
+};
+
+const showPages = () => {
   let initialIndex = (page - 1) * elementsPerPage;
   let finalIndex = page * elementsPerPage;
+  let cont = 1;
   tableBody.innerHTML = "";
-
-  console.log(finalIndex);
+  // console.log(initialIndex, finalIndex);
 
   for (let i = initialIndex; i < finalIndex; i++) {
     if (i < item[0].length) {
       const elementTr = document.createElement("tr");
       const elementTh = document.createElement("th");
       elementTh.setAttribute("scope", "row");
-      const elementTd = document.createElement("td");
-      elementTd.textContent = item[0][i];
-      elementTr.appendChild(elementTh);
-      elementTr.appendChild(elementTd);
+
+      const textContent = Object.values(item[0][i]).toString();
+      let textArray = textContent.split(" ");
+      const formatArray = (textArray = textContent.split(","));
+
+      formatArray.forEach((text) => {
+        if (
+          cont === Object.keys(item[0][i]).length &&
+          actualPage !== "departamento"
+        ) {
+          cont = 1;
+        } else {
+          const elementTd = document.createElement("td");
+          elementTd.textContent = text;
+          elementTr.appendChild(elementTd);
+          cont++;
+        }
+      });
+
       tableBody.appendChild(elementTr);
     }
   }
@@ -128,15 +156,66 @@ navItems.forEach((item) => {
         insertTableHeader(data);
         eraseTableContent();
         insertTableContent();
+        setCurrentPage(1)
+        // console.log(totalPages)
+      })
+      .catch((err) => {
+        eraseTableContent();
+        eraseTableHeader();
+        //alert("No hay datos para mostrar");
+        
+        const div = document.createElement("div");
+        div.setAttribute("class", "alert alert-danger alert-dismissible fade show");
+        div.setAttribute("role", "alert");
+        const strong = document.createElement("strong");
+        strong.textContent = "Aviso!! No hay ningún dato para mostrar";
+        const button = document.createElement("button");
+        button.setAttribute("type", "button");
+        button.setAttribute("class", "btn-close");
+        button.setAttribute("data-bs-dismiss", "alert");
+        button.setAttribute("aria-label", "Close");
+        navPagination.setAttribute("hidden", true)
+        div.appendChild(strong);
+        div.appendChild(button);
+        tableBody.appendChild(div);
+        
+        setTimeout(() => {
+          tableHead.removeChild(div);
+        }, 3500);
       });
+
+      navPagination.removeAttribute("hidden")
   });
+});
+
+prevButton.addEventListener("click", () => {
+  if (page === 1) return;
+  setCurrentPage(page - 1)
+});
+
+nextButton.addEventListener("click", () => {
+  if(page === totalPages) return;
+  setCurrentPage(page + 1)
 });
 
 // Coloca el evento a cada elemento de la barra de paginación DEPURAR
 paginationButton.forEach((button) => {
   button.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log(e.target);
+    //  console.log(page);
     showPages();
   });
 });
+
+btnAdmin.addEventListener("click", (e) => {
+  window.location.href = "http://localhost:8000/admin"
+})
+
+btnLogin.addEventListener("click", (e) => {
+  e.preventDefault();
+  if( userForm.value === "leinor" || userForm.value === "desi" && passwordForm.value === "1234"){
+    loginSection.setAttribute("hidden", true)
+    mainSection.removeAttribute("hidden")
+  }
+  else alert("Credenciales incorrectas")
+})
